7ac45e976f4bdf211838df89068c8e955f7b41da
sadvika <sadvika@igreensystems.com>
2022-12-20 11:45:50 +0530

    jquery added

--- sadhvika/forms.js
+++ sadhvika/forms.js
@@ -1,18 +1,19 @@
-let myForm = document.getElementById("myForm");
-let yesEl = document.getElementById("yes");
-let NoEl = document.getElementById("No");
-let yesblock = document.getElementById("yesblock");
-let noblock = document.getElementById("noblock");
-let selectedOption = null;
-yesEl.addEventListener("click", function(event) {
-    yesblock.classList.remove("d-none");
-    yesblock.classList.add("d-block");
-    noblock.classList.remove("d-block");
-    noblock.classList.add("d-none");
-});
-NoEl.addEventListener("click", function(event) {
-    yesblock.classList.remove("d-block")
-    yesblock.classList.add("d-none");
-    noblock.classList.remove("d-none");
-    noblock.classList.add("d-block");
-});
+// let myForm = document.getElementById("myForm");
+// let yesEl = document.getElementById("yes");
+// let NoEl = document.getElementById("No");
+// let yesblock = document.getElementById("yesblock");
+// let noblock = document.getElementById("noblock");
+// let selectedOption = null;
+// yesEl.addEventListener("click", function(event) {
+//     yesblock.classList.remove("d-none");
+//     yesblock.classList.add("d-block");
+//     noblock.classList.remove("d-block");
+//     noblock.classList.add("d-none");
+// });
+// NoEl.addEventListener("click", function(event) {
+//     yesblock.classList.remove("d-block")
+//     yesblock.classList.add("d-none");
+//     noblock.classList.remove("d-none");
+//     noblock.classList.add("d-block");
+// });
+
