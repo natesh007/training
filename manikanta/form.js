d7604967ca24892030735b9e0cf969f14a70b0d1
manikanta8712 <manikanta@igreensystems.com>
2022-12-21 16:54:31 +0530

    addmore created

--- manikanta/form.js
+++ manikanta/form.js
@@ -35,104 +35,96 @@ $('#myform').validate({
         },
         inlineRadioOptions: "required",
         colName: {
-            required : function(element)
-            {
-                if($('input[name="inlineRadioOptions"]:checked').val() == 'yes')
-                {
-                    if($('#colName').val() == '')
-                    {
+            required: function (element) {
+                if ($('input[name="inlineRadioOptions"]:checked').val() == 'yes') {
+                    if ($('#colName').val() == '') {
                         return true;
                     }
-                    else
-                    {
+                    else {
                         return false;
                     }
                 }
-                else
-                        {
-                            return false;
-                        }
+                else {
+                    return false;
+                }
             }
         },
         styear: {
-            required : function(element)
-            {
-                if($('input[name="inlineRadioOptions"]:checked').val() == 'yes')
-                {
-                    if($('#exampleInputstartyear').val() == '')
-                    {
+            required: function (element) {
+                if ($('input[name="inlineRadioOptions"]:checked').val() == 'yes') {
+                    if ($('#exampleInputstartyear').val() == '') {
                         return true;
                     }
-                    else
-                    {
+                    else {
                         return false;
                     }
                 }
-                else
-                        {
-                            return false;
-                        }
+                else {
+                    return false;
+                }
             }
         },
         endyear: {
-            required : function(element)
-            {
-                if($('input[name="inlineRadioOptions"]:checked').val() == 'yes')
-                {
-                    if($('#exampleInputendyear').val() == '')
-                    {
+            required: function (element) {
+                if ($('input[name="inlineRadioOptions"]:checked').val() == 'yes') {
+                    if ($('#exampleInputendyear').val() == '') {
                         return true;
                     }
-                    else
-                    {
+                    else {
                         return false;
                     }
                 }
-                else
-                        {
-                            return false;
-                        }
+                else {
+                    return false;
+                }
             }
         },
         course: {
-            required : function(element)
-            {
-                if($('input[name="inlineRadioOptions"]:checked').val() == 'yes')
-                {
-                    if($('#exampleInputcourseName').val() == '')
-                    {
+            required: function (element) {
+                if ($('input[name="inlineRadioOptions"]:checked').val() == 'yes') {
+                    if ($('#exampleInputcourseName').val() == '') {
                         return true;
                     }
-                    else
-                    {
+                    else {
                         return false;
                     }
                 }
-                else
-                        {
-                            return false;
-                        }
+                else {
+                    return false;
+                }
             }
         },
         textarea: {
-            required : function(element)
-            {
-                if($('input[name="inlineRadioOptions"]:checked').val() == 'no')
-                {
-                    if($('#textarea').val() == '')
-                    {
+            required: function (element) {
+                if ($('input[name="inlineRadioOptions"]:checked').val() == 'no') {
+                    if ($('#textarea').val() == '') {
                         return true;
                     }
-                    else
-                    {
+                    else {
                         return false;
                     }
                 }
-                else
-                        {
-                            return false;
-                        }
+                else {
+                    return false;
+                }
             }
+        },
+        mytext : {
+            required :(function(){
+                
+              
+              var addlist_button = $('input[name=addlist]');
+                addlist_button.on('click', function(event){
+                  
+                  var skill_name = $('input[name=mytext]').val();
+                  if(skill_name == ""){
+                    $('.show_hide').hide();;
+                  }
+                  else{
+                    $('.show_hide').show();
+                  }
+                });
+              })()
         }
     },
     messages: {
@@ -164,6 +156,10 @@ $('#myform').validate({
             required: "please enter course",
         },
         textarea: "please enter details",
+        mytext : {
+            required : "please enter your skills",
+           
+        }
     },
     errorPlacement:
         function (error, element) {
@@ -174,5 +170,33 @@ $('#myform').validate({
             else {
                 error.insertAfter(element);
             }
-        },
+        }
+});
+// Add more
+// $(function() {
+//     $("#addMore").click(function(e) {
+//       e.preventDefault();
+//       $("#fieldList").append("<div>&nbsp;</div>");
+//       $("#fieldList").append("<input type='text' name='skills[]' placeholder='skills' />");
+//     //   $("#fieldList").append("<li><input type='text' name='skills[]' placeholder='skills' /></li>");
+//     //   $("#fieldList").append("<li><input type='text' name='skills[]' placeholder='skills' /></li>");
+//     });
+//   });
+$(document).ready(function () {
+    var max_fields = 10; //maximum input boxes allowed
+    var wrapper = $(".input_fields_wrap"); //Fields wrapper
+    var add_button = $(".add_field_button"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function (e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div class="show_hide"><input type="text" placeholder="skills" name="skills"/><btn  class="remove_field">Remove</btn></div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function (e) { //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
 });
